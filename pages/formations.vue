98042a458dd3d9ae1b7a5913d6c9363d00ad9ea8
thedrewen <louis.charneux@gmail.com>
2024-10-15 18:45:07 +0200

    base

---
+++ pages/formations.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    setup() {
+        const route = useRoute();
+        return {
+            route
+        }
+    }
+}
+</script>
+
+<template>
+    formations
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    setup() {
+        const route = useRoute();
+        return {
+            route
+        }
+    }
+}
+</script>
+
+<template>
+    
+</template>
+
+<style scoped>
+    
+</style>
